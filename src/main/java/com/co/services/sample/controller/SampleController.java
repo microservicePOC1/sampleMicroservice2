dded641f311c927c05f7a65c41ccc053a3d76e96
charley43613 <41710701+charley43613@users.noreply.github.com>
2022-10-18 16:48:31 -0400

    Update SampleController.java

--- src/main/java/com/co/services/sample/controller/SampleController.java
+++ src/main/java/com/co/services/sample/controller/SampleController.java
@@ -16,41 +16,8 @@ import com.azure.security.keyvault.secrets.models.KeyVaultSecret;
 public class SampleController {
     
     @GetMapping("/")
-    public String sampleApi() throws InterruptedException, IllegalArgumentException {
-        String keyVaultName = System.getenv("KEY_VAULT_NAME");
-        //secret start
-        String keyVaultUri = "https://" + keyVaultName + ".vault.azure.net";
+    public String sampleApi() {
 
-        System.out.printf("key vault name = %s and key vault URI = %s \n", keyVaultName, keyVaultUri);
-
-        SecretClient secretClient = new SecretClientBuilder()
-            .vaultUrl(keyVaultUri)
-            .credential(new DefaultAzureCredentialBuilder().build())
-            .buildClient();
-
-
-        String secretName = "mySecret";
-
-        System.out.println("Please provide the value of your secret > ");
-        
-        String secretValue = "SampleSecretValue!";
-
-        System.out.print("Creating a secret in " + keyVaultName + " called '" + secretName + "' with value '" + secretValue + "` ... ");
-
-        secretClient.setSecret(new KeyVaultSecret(secretName, secretValue));
-
-        System.out.println("Retrieving your secret from " + keyVaultName + ".");
-
-        KeyVaultSecret retrievedSecret = secretClient.getSecret(secretName);
-
-        System.out.println("Your secret's value is '" + retrievedSecret.getValue() + "'.");
-        System.out.print("Deleting your secret from " + keyVaultName + " ... ");
-
-        SyncPoller<DeletedSecret, Void> deletionPoller = secretClient.beginDeleteSecret(secretName);
-        deletionPoller.waitForCompletion();
-
-        System.out.println("done.");
-        //secret end
         return "Hello Sample Microservice 2";
     }
     

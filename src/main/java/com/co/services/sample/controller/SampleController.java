00fe28a3db8633af9fb37f990601e8e44ee347e1
charley43613 <41710701+charley43613@users.noreply.github.com>
2022-10-18 16:09:24 -0400

    Update SampleController.java

--- src/main/java/com/co/services/sample/controller/SampleController.java
+++ src/main/java/com/co/services/sample/controller/SampleController.java
@@ -2,13 +2,55 @@ package com.co.services.sample.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.io.Console;
+
+import com.azure.core.util.polling.SyncPoller;
+import com.azure.identity.DefaultAzureCredentialBuilder;
+
+import com.azure.security.keyvault.secrets.SecretClient;
+import com.azure.security.keyvault.secrets.SecretClientBuilder;
+import com.azure.security.keyvault.secrets.models.DeletedSecret;
+import com.azure.security.keyvault.secrets.models.KeyVaultSecret;
 
 @RestController
 public class SampleController {
     
     @GetMapping("/")
-    public String sampleApi() {
+    public String sampleApi() throws InterruptedException, IllegalArgumentException {
+        String keyVaultName = System.getenv("KEY_VAULT_NAME");
+        //secret start
+        String keyVaultUri = "https://" + keyVaultName + ".vault.azure.net";
 
+        System.out.printf("key vault name = %s and key vault URI = %s \n", keyVaultName, keyVaultUri);
+
+        SecretClient secretClient = new SecretClientBuilder()
+            .vaultUrl(keyVaultUri)
+            .credential(new DefaultAzureCredentialBuilder().build())
+            .buildClient();
+
+
+        String secretName = "mySecret";
+
+        System.out.println("Please provide the value of your secret > ");
+        
+        String secretValue = "SampleSecretValue!";
+
+        System.out.print("Creating a secret in " + keyVaultName + " called '" + secretName + "' with value '" + secretValue + "` ... ");
+
+        secretClient.setSecret(new KeyVaultSecret(secretName, secretValue));
+
+        System.out.println("Retrieving your secret from " + keyVaultName + ".");
+
+        KeyVaultSecret retrievedSecret = secretClient.getSecret(secretName);
+
+        System.out.println("Your secret's value is '" + retrievedSecret.getValue() + "'.");
+        System.out.print("Deleting your secret from " + keyVaultName + " ... ");
+
+        SyncPoller<DeletedSecret, Void> deletionPoller = secretClient.beginDeleteSecret(secretName);
+        deletionPoller.waitForCompletion();
+
+        System.out.println("done.");
+        //secret end
         return "Hello Sample Microservice 2";
     }
     
